f2797b4361ebf733c4e3e7b1acbe660ec79bca04
xie ren qiang <1173044198@qq.com>
2020-07-08 22:47:33 +0800

    MOD:rm .DS_Store

---
+++ RQMediator/RQMediator/RQMediator.h
@@ -0,0 +1,20 @@
+//
+//  RQMediator.h
+//  RQMediator
+//
+//  Created by 谢仁强 on 2020/7/8.
+//  Copyright © 2020 谢仁强. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RQMediator : NSObject
+
++ (instancetype)sharedMediator;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RQMediator/RQMediator/RQMediator.m
@@ -0,0 +1,13 @@
+//
+//  RQMediator.m
+//  RQMediator
+//
+//  Created by 谢仁强 on 2020/7/8.
+//  Copyright © 2020 谢仁强. All rights reserved.
+//
+
+#import "RQMediator.h"
+
+@implementation RQMediator
+
+@end
